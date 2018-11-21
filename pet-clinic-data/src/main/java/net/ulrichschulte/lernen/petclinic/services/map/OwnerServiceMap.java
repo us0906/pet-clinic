dc85a98f0fb417de2cce150cecd96f5546a4d2ca
gaukler69 <ulrich@ulrich-schulte.net>
2018-11-21 21:03:58 +0100

    MapServices

---
+++ pet-clinic-data/src/main/java/net/ulrichschulte/lernen/petclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,35 @@
+package net.ulrichschulte.lernen.petclinic.services.map;
+
+import net.ulrichschulte.lernen.petclinic.model.Owner;
+import net.ulrichschulte.lernen.petclinic.services.CrudService;
+
+import javax.sql.rowset.CachedRowSet;
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
+    @Override
+    public Set<Owner> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Owner findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Owner save(Owner object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/net/ulrichschulte/lernen/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package net.ulrichschulte.lernen.petclinic.services.map;
+
+import net.ulrichschulte.lernen.petclinic.model.Pet;
+import net.ulrichschulte.lernen.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
