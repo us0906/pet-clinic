71b2ad20415f14735ad78361d87cebdfa49f4ad8
Ulrich <ulrich@ulrich-schulte.net>
2018-12-05 20:41:47 +0100

    internationalization

--- pet-clinic-web/src/main/java/net/ulrichschulte/lernen/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/net/ulrichschulte/lernen/petclinic/PetClinicApplication.java
@@ -2,6 +2,7 @@ package net.ulrichschulte.lernen.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
