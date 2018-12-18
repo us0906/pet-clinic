b6dd0cd8e7676e26b81581f6623a3c11580226de
Ulrich <ulrich@ulrich-schulte.net>
2018-12-18 22:16:31 +0100

    PetTypeService

---
+++ pet-clinic-data/src/main/java/net/ulrichschulte/lernen/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package net.ulrichschulte.lernen.petclinic.services;
+
+import net.ulrichschulte.lernen.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/net/ulrichschulte/lernen/petclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,43 @@
+package net.ulrichschulte.lernen.petclinic.services.map;
+
+import net.ulrichschulte.lernen.petclinic.model.PetType;
+import net.ulrichschulte.lernen.petclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long>  implements  PetTypeService{
+
+
+    @Override
+    public Set<PetType> findAll() {
+
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+
+        super.deleteById(id);
+    }
+
+}
