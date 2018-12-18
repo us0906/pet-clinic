1fbf25ce709cdbcbd6873e67c14083bd8548a991
Ulrich <ulrich@ulrich-schulte.net>
2018-12-18 22:00:41 +0100

    specialities

--- pet-clinic-data/src/main/java/net/ulrichschulte/lernen/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/net/ulrichschulte/lernen/petclinic/model/Vet.java
@@ -4,4 +4,14 @@ import org.springframework.context.annotation.Bean;
 
 
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
