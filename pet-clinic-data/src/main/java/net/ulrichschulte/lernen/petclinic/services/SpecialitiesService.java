6bcd9919c2892c660fc6a48302b7cdb8e0e8df5f
Ulrich <ulrich@ulrich-schulte.net>
2018-12-28 11:36:52 +0100

    SpecialitesService

---
+++ pet-clinic-data/src/main/java/net/ulrichschulte/lernen/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package net.ulrichschulte.lernen.petclinic.services;
+
+import net.ulrichschulte.lernen.petclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+
+}
